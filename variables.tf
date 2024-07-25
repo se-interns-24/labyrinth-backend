303b32d598b53da5a29c60a9383476822357f2f3
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-25 16:07:18 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "tfc_org_name" {
+  description = "Name of the Terraform Cloud Organization"
+  type        = string
+  default     = "se-intern-project"
+}
+
+variable "tfc_network_workspace_name" {
+  description = "Name of the network workspace"
+  type        = string
+  default     = "labyrinth-network-dev"
+}
