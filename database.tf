99e88da9fd083bfb5bc1d75e04b7479ea20ce302
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-25 15:42:27 -0500

    Create database.tf

---
+++ database.tf
@@ -0,0 +1,18 @@
+resource "aws_db_instance" "labyrinth-db" {
+  allocated_storage                     = 20
+  db_subnet_group_name                  = data.terraform_remote_state.network.outputs.subnet_group_name
+  engine                                = "mysql"
+  engine_version                        = "8.0.35"
+  identifier                            = "labyrinth-db"
+  instance_class                        = "db.m6gd.large"
+  parameter_group_name                  = "default.mysql8.0"
+  password                              = null # sensitive
+  publicly_accessible                   = false
+  skip_final_snapshot                   = true
+  username                              = "admin"
+  vpc_security_group_ids                = data.terraform_remote_state.network.outputs.rds_security_group_id
+
+  tags = {
+    Name = "Labyrinth Database"
+  }
+}
