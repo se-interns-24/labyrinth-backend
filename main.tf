575e91136603a5e6b623307eac9ac61a1464511d
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-31 16:10:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,16 @@ data "terraform_remote_state" "network" {
   }
 }
 
-data "hcp_vault_secrets_secret" "labyrinth" {
-  app_name = "labyrinth"
+# Data block to retrieve the username from Vault Secrets
+data "hcp_vault_secrets_secret" "username" {
+  app_name    = "labyrinth"
   secret_name = "username"
+}
+
+# Data block to retrieve the password from Vault Secrets
+data "hcp_vault_secrets_secret" "password" {
+  app_name    = "labyrinth"
+  secret_name = "password"
 }
 
 resource "aws_db_instance" "labyrinth-db" {
@@ -29,10 +36,10 @@ resource "aws_db_instance" "labyrinth-db" {
   identifier                            = "labyrinth-db"
   instance_class                        = "db.m6gd.large"
   parameter_group_name                  = "default.mysql8.0"
-  password                              = data.hcp_vault_secrets_app.labyrinth.password
+  password                              = ata.hcp_vault_secrets_secret.password.secret_value
   publicly_accessible                   = false
   skip_final_snapshot                   = true
-  username                              = data.hcp_vault_secrets_secret.labyrinth.username
+  username                              = data.hcp_vault_secrets_secret.username.secret_value
   vpc_security_group_ids                = data.terraform_remote_state.network.outputs.rds_security_group_id
 
   tags = {
