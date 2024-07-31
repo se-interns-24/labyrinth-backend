13915439e37b37acdf0d4314002b573a1d250b93
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-31 16:13:03 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,3 @@ value = aws_db_instance.labyrinth-db.port
 output "db_instance_name" { 
 value = aws_db_instance.labyrinth-db.db_name 
 } 
-
-output "db_instance_username" { 
-value = aws_db_instance.labyrinth-db.username 
-}
