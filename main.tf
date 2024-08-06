131f1dea7201a73bd404180bd0491589e862956c
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-08-05 20:25:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,14 +18,14 @@ data "terraform_remote_state" "network" {
 
 # Data block to retrieve the username from Vault Secrets
 data "hcp_vault_secrets_secret" "username" {
-  app_name    = "labyrinth"
-  secret_name = "username"
+  app_name    = var.hvs_app_name
+  secret_name = var.username_secret
 }
 
 # Data block to retrieve the password from Vault Secrets
 data "hcp_vault_secrets_secret" "password" {
-  app_name    = "labyrinth"
-  secret_name = "password"
+  app_name    = var.hvs_app_name
+  secret_name = var.password_secret
 }
 
 resource "aws_db_instance" "labyrinth-db" {
@@ -33,7 +33,7 @@ resource "aws_db_instance" "labyrinth-db" {
   db_subnet_group_name                  = data.terraform_remote_state.network.outputs.subnet_group_name
   engine                                = "mysql"
   engine_version                        = "8.0.35"
-  identifier                            = "labyrinth-db"
+  identifier                            = var.database
   instance_class                        = "db.m6gd.large"
   parameter_group_name                  = "default.mysql8.0"
   password                              = data.hcp_vault_secrets_secret.password.secret_value
