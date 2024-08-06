3075f7352c0deff77393177c5771cb69f0018c87
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-08-05 20:06:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,23 @@ variable "tfc_network_workspace_name" {
   description = "Name of the network workspace"
   type        = string
 }
+
+variable "hvs_app_name" {
+  description = "Name of HVS app"
+  default     = "labyrinth"
+}
+
+variable "username_secret" {
+  description = "Name of the secret"
+  default     = "username"
+}
+
+variable "password_secret" {
+  description = "Name of the secret"
+  default     = "password"
+}
+
+variable "database" {
+  description = "Identifier of the db"
+  default     = "labyrinth-db"
+}
